a69931a542a5ef407caddfe1f2c59da5cb95b974
DEVRHEL <haovobaomat@gmail.com>
2020-07-26 09:16:52 +0700

    first API Project

---
+++ demo/src/main/java/com/example/demo/exception/ErrorResponse.java
@@ -0,0 +1,37 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+	private HttpStatus status;
+	
+	private String message;
+	
+	public ErrorResponse() {
+		
+	}
+
+	public ErrorResponse(HttpStatus status, String message) {
+		super();
+		this.status = status;
+		this.message = message;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
