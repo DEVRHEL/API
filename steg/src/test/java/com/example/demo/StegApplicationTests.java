ac917d1d3b25e400a96aa68f60f013e26d9598e1
DEVRHEL <haovobaomat@gmail.com>
2020-09-30 20:16:19 +0700

    add steg

---
+++ steg/src/test/java/com/example/demo/StegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
